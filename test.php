b5e6f13a8be3e701e5065473dfa787a97acbdd90
Rajkumar Parihar <55016311+RajP7Jowa@users.noreply.github.com>
2020-04-09 10:18:52 +0530

    Create test.php

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php  
+
+$fp = fopen('data.txt', 'a');//opens file in append mode  
+
+fwrite($fp, ' this is additional text ');  
+
+fwrite($fp, 'appending data');  
+
+fclose($fp);  
+
+  
+
+echo "File appended successfully";  
+
+?>  
